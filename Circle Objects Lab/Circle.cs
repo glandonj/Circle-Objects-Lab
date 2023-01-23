a7b2f6b7a72e140628703ccb17079e97e69ff494
john <glandonj@gmail.com>
2023-01-23 17:36:19 -0500

    initial commit

---
+++ Circle Objects Lab/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+
+namespace Circle_Objects_Lab
+{
+	public class Circle
+	{
+        public double radius { get; set; }
+        //public double circumferance { get; set; }
+        //public decimal area { get; set; }
+        //Constructor
+        public Circle (double _radius)
+        {
+            radius = _radius;
+        }
+
+        //Methods
+        public double CalculateCircumference()
+        {
+            return Math.PI * radius * 2;
+        }
+        public double CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public double CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        private double FormatNumber (double x)
+        {
+            return Math.Round(x, 2);
+        }
+        public string GetDetails()
+        {
+            return String.Format("{0,-15} {1,-15} {2, -15}", $"{radius}", $"{CalculateFormattedCircumference()}", $"{CalculateFormattedArea()}");
+        }
+    }
+}
+
---
+++ Circle Objects Lab/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Circle_Objects_Lab;
+
+List<Circle> circles = new List<Circle>();
+
+Console.WriteLine("Welcome to the Circle Circumference and Area Calculator.");
+
+//loop program
+bool runProgram = true;
+while (runProgram)
+{
+    double radius = 0;
+    Console.Write("Please enter a radius (decimals acceptable): ");
+    while (double.TryParse(Console.ReadLine(), out radius) == false)
+    {
+        Console.WriteLine("Not a number. Try again.");
+    }
+    int counter = 0;
+
+    circles.Add(new Circle(radius));
+    //Console.WriteLine($"Circle with radius {radius} added.");
+    Console.Clear();
+    Console.WriteLine(String.Format("{0,-15} {1,-15} {2, -15}", "Radius", "Circumference", "Area"));
+    Console.WriteLine(String.Format("{0,-15} {1,-15} {2, -15}", "===========", "===========", "==========="));
+
+    foreach (Circle c in circles)
+    {
+        Console.WriteLine(c.GetDetails());
+        counter++;
+    }
+    while (true)
+    {
+        Console.WriteLine("Do you want to continue? y/n");
+        string choice = Console.ReadLine().ToLower().Trim();
+        if (choice == "y")
+        {
+            runProgram = true;
+            break;
+        }
+        else if (choice == "n")
+        {
+            runProgram = false;
+            Console.WriteLine($"You calculated {counter} circles. Goodbye!");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
+
+Console.ReadLine();
